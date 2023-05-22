2dc699c007a2cc9bf54d82270b4eb7a4ffbbd8df
Hussomuss <hussain_jabbar1@hotmail.com>
2023-05-22 22:47:20 +0100

    second commit

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,18 +1,54 @@
-import { useState } from "react";
+"use client";
 
+import { useState, FormEventHandler } from "react";
 import { TiPlus } from "react-icons/ti";
 import Modal from "./Modal";
+import { addTodo } from "@/apis";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 function AddTask() {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTextValue, setNewTextValue] = useState<string>("");
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await addTodo({
+      id: uuidv4(),
+      text: newTextValue,
+    });
+    setNewTextValue("");
+    setModalOpen(false);
+    router.refresh();
+  };
 
   return (
     <div>
-      <button className="btn btn-primary w-full">
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
         Add new task <TiPlus className="ml-4" size={20} />
       </button>
 
-      <Modal />
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form onSubmit={handleSubmitNewTodo}>
+          <h3 className="font-bold text-lg text-yellow-600">Add New Task</h3>
+          <div className="modal-action">
+            <input
+              value={newTextValue}
+              onChange={(e) => setNewTextValue(e.target.value)}
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
